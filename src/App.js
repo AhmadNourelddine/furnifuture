789b92efab0d9b5636509f78ea9a1f15a4ac5bd7
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-02-28 21:23:57 +0200

    added about page in route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./layout/Main";
 import {Routes, Route} from 'react-router-dom';
 import Navbar from "./components/homeNavbar";
 import HomeCarousel from "./components/carousel";
+import About from "./pages/about";
 import Login from "./pages/Login";
 import SignUp from "./pages/Signup";
 
@@ -13,8 +14,8 @@ function App() {
       { 
         <div>
           <Routes>
-          <Route exact path="/" element={<HomeCarousel/>} />
-          <Route exact path="/about" element={<HomeCarousel/>} />
+          <Route exact path="/" element={<About/>} />
+          <Route exact path="/about" element={<About/>} />
           <Route exact path="/login" element={<Login/>} />
           <Route exact path="/signup" element={<SignUp/>} />
           </Routes>
