254b1c19ede1d7a4864e4a0a0ba9647c584e5537
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-02-28 21:23:28 +0200

    added about page

---
+++ src/pages/about/index.js
@@ -0,0 +1,14 @@
+import HomeCarousel from "../../components/carousel";
+import Footer from "../../components/footer";
+
+const About = ()=>{
+    return(
+        <div>
+
+        <HomeCarousel />
+        <Footer />
+                    
+        </div>
+    );
+}
+export default About;
