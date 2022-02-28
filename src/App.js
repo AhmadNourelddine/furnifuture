2c0711a1496ba98a7b77c8fb7d90425442ef70cc
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-02-28 20:38:51 +0200

    added routes for main page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import Main from "./layout/Main";
+import {Routes, Route} from 'react-router-dom';
+import Navbar from "./components/homeNavbar";
+import HomeCarousel from "./components/carousel";
+import Login from "./pages/Login";
+import SignUp from "./pages/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main>
+      { 
+        <div>
+          <Routes>
+          <Route exact path="/" element={<HomeCarousel/>} />
+          <Route exact path="/about" element={<HomeCarousel/>} />
+          <Route exact path="/login" element={<Login/>} />
+          <Route exact path="/signup" element={<SignUp/>} />
+          </Routes>
+        </div>
+      }
+    </Main>
   );
 }
 
