49d4159b98019b21299960b7396acfa7eae3d8ed
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-03-01 14:21:00 +0200

    chnaged position of footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,7 +3,7 @@ import Typography from '@mui/material/Typography';
 
 const Footer = ()=>{
     return(
-        <div style={{position:"fixed", width:"100%", backgroundColor:"black", color:"white", bottom:"0"}}>
+        <div style={{ width:"100%", backgroundColor:"black", color:"white"}}>
            <div style={{padding:"2%", display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
            <Typography>Contact Us</Typography>
             <Typography>Email</Typography>
