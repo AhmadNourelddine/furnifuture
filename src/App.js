43c3815fa630d6a474ba067f1ff6f6c878e67ab0
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-03-02 21:29:53 +0200

    fix footer position

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Main from "./layout/Main";
 import {Routes, Route} from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import About from "./pages/about";
 import Login from "./pages/Login";
 import SignUp from "./pages/Signup";
@@ -18,13 +19,27 @@ function App() {
           <Route exact path="/about" element={<About/>} />
           <Route exact path="/login" element={<Login/>} />
           <Route exact path="/signup" element={<SignUp/>} />
-          <Route exact path="/dashboard" element={<Dashboard/>} />
+          {/* <Route exact path="/dashboard" element={<Dashboard/>} /> */}
           <Route exact path="/contact-us" element={<ContactUs/>} />
+          <Route
+            path="/dashboard"
+            element={
+              <RequireAuth redirectTo="/dashboard">
+                <Dashboard />
+              </RequireAuth>
+            }
+      />
           </Routes>
         </div>
       }
     </Main>
   );
+}
+
+function RequireAuth({ children, redirectTo }) {
+  let Navigate = useNavigate();
+  let isAuthenticated = window.localStorage.getItem('authToken');
+  return isAuthenticated ? children : <Navigate to={redirectTo} />;
 }
 
 export default App;
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -3,11 +3,13 @@ import Footer from "../../components/footer";
 
 const About = ()=>{
     return(
-        <div>
+        <div style={{display:"flex", flexDirection:"column"}}>
 
+        <div style={{minHeight:"calc( 100vh - 68.5px)", flexGrow:"1"}}>
         <HomeCarousel/>
+        </div>
 
-        <div style={{width:"100%", backgroundColor:"black", color:"white"}}>
+        <div style={{width:"100%", backgroundColor:"black", color:"white", alignSelf:"end"}}>
         <Footer />
         </div>
           
