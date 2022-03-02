caabb59d9dfcfe92d53677459e8a826e8bf36889
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-03-02 11:25:35 +0200

    added onclick and navigate function

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
+import { useNavigate } from "react-router-dom";
 
 const Footer = ()=>{
+
+    let Navigate = useNavigate()
+
+    const navigateContactUs = ()=>{
+
+        Navigate('/contact-us')
+    }
+
     return(
         <div style={{ width:"100%", backgroundColor:"black", color:"white"}}>
            <div style={{padding:"2%", display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
-           <Typography>Contact Us</Typography>
+           <Typography onClick={navigateContactUs}>Contact Us</Typography>
             <Typography>Email</Typography>
             <Typography>Phone Number</Typography>
            </div>
