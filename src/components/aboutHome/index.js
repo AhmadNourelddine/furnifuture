2f88d46cf343ab5b86673f1c3b434fc3a67c6e8f
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-03-10 20:56:11 +0200

    set about page html and css

---
+++ src/components/aboutHome/index.js
@@ -0,0 +1,29 @@
+import { Button, Typography } from '@mui/material';
+import React from 'react';
+import '../../css/aboutHome/aboutHome.css';
+
+const AboutHome = ()=>{
+
+    return(
+        <div id='about-page-home'>
+            <div id='about-welcome'>
+                <Typography className="about-welcome-to-the">WELCOME TO THE </Typography>
+                <Typography className="about-welcome-furnifuture">FURNIFUTURE </Typography>
+            </div>
+            <div id='about-getstarted'>
+                <div id='about-buy-sell-getstarted'>
+                    <p className="getstarted-title">Buy & Sell Furniture</p>
+                    <p className="getstarted-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <Button className="getstarted-buysell-button">Get Started</Button>
+                </div>
+                <div id='about-deliver-getstarted'>
+                    <p className="getstarted-title">Deliver Furniture</p>
+                    <p className="getstarted-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <Button className="getstarted-delivery-button">Get Started</Button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AboutHome;
---
+++ src/pages/about/index.js
@@ -0,0 +1,21 @@
+import AboutHome from "../../components/aboutHome";
+import HomeCarousel from "../../components/carousel";
+import Footer from "../../components/footer";
+
+const About = ()=>{
+    return(
+        <div style={{display:"flex", flexDirection:"column"}}>
+
+        {/* <div style={{minHeight:"calc( 100vh - 68.5px)", flexGrow:"1"}}>
+        <HomeCarousel/>
+        </div> */}
+        <AboutHome />
+
+        <div style={{width:"100%", backgroundColor:"black", color:"white", alignSelf:"end"}}>
+        <Footer />
+        </div>
+          
+        </div>
+    );
+}
+export default About;
