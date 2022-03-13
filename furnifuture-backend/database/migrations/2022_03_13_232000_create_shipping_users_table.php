ec8acecbc3ad3285f13800c26116c6add2ae10db
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-03-14 01:38:26 +0200

    shipping user model and migration

---
+++ furnifuture-backend/database/migrations/2022_03_13_232000_create_shipping_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShippingUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shipping_users', function (Blueprint $table) {
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('phone_number');
+            $table->string('location');
+            $table->string('vehicle_load');
+            $table->boolean('is_shipping_user');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shipping_users');
+    }
+}
