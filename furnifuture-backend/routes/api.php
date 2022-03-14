679d224cd0efeda700bdd47fd908b837d86cf3ba
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-03-14 17:59:21 +0200

    added route for save product api

--- furnifuture-backend/routes/api.php
+++ furnifuture-backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CartController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,10 +20,13 @@ Route::group([
 ], function ($router) {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/registerShipping', [AuthController::class, 'registerShipping']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
     Route::get('/user-profile', [AuthController::class, 'userProfile']);    
 });
+
+Route::post('/saveProduct-toCart', [CartController::class, 'saveProduct']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
