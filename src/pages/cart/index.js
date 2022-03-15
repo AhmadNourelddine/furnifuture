8a7839d3b66becdcb015d1cdc2806d4d01804e9c
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-03-15 23:21:35 +0200

    added button change color on click

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Typography } from '@mui/material';
 import { TextField } from '@material-ui/core';
 import '../../css/cart/cart.css';
@@ -6,6 +6,27 @@ import FurnitureItem from '../../components/furnitureItem';
 
 const Cart = ()=>{
 
+    const[ClickShipping, setClickShipping]= useState(false);
+    const[ClickSavedFurniture, setClickSavedFurniture]= useState(true);
+
+    const[savedFurnitureColor, setsavedFurnitureColor]= useState('#6F1A07'); 
+    const[shippingColor, setshippingColor]= useState('#B4A89E');
+
+    useEffect(() => {
+
+        if(ClickShipping)
+        {
+            setshippingColor('#6F1A07');
+            setsavedFurnitureColor('#B4A89E');
+            setClickSavedFurniture(false);
+        }
+        if(ClickSavedFurniture){
+            setshippingColor('#B4A89E');
+            setsavedFurnitureColor('#6F1A07');
+            setClickShipping(false);
+        }
+
+      },[ClickShipping, ClickSavedFurniture]);
 
     return(
         <div className='cart-page'>
@@ -14,8 +35,13 @@ const Cart = ()=>{
 
             <Typography className='cart-page-title'>Your Cart</Typography>
             <div className='cart-page-navbar'>
-                <Button className='cart-page-navbar-btn'>Saved Furniture</Button>
-                <Button className='cart-page-navbar-btn'>Shipping</Button>
+                <Button className='cart-page-navbar-btn'
+                style={{backgroundColor:savedFurnitureColor}}
+                onClick={()=>{setClickSavedFurniture(true)}}>Saved Furniture</Button>
+
+                <Button className='cart-page-navbar-btn' 
+                style={{backgroundColor:shippingColor}}
+                onClick={()=>{setClickShipping(true)}}>Shipping</Button>
             </div>
 
             </div>
