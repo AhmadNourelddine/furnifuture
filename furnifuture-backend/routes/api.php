ee3b5c1dd20477497533a6677ebf5bae9b56f670
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-03-15 03:22:39 +0200

    grouped routes

--- furnifuture-backend/routes/api.php
+++ furnifuture-backend/routes/api.php
@@ -5,16 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
+
 Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
@@ -22,17 +14,36 @@ Route::group([
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/registerShipping', [AuthController::class, 'registerShipping']);
+    Route::post('/update-profile', [AuthController::class, 'updateProfile']);
+    Route::post('/update-shipping-profile', [AuthController::class, 'updateProfileShipping']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
     Route::get('/user-profile', [AuthController::class, 'userProfile']);    
 });
 
-Route::post('/search-product', [ProductController::class, 'searchProduct']);
-Route::post('/sell-product', [ProductController::class, 'sellProduct']);
-Route::post('/edit-product', [ProductController::class, 'editProduct']);
-Route::post('/saveProduct-toCart', [CartController::class, 'saveProduct']);
-Route::post('/removeProduct-fromCart', [CartController::class, 'removeProduct']);
-Route::post('/user-products', [ProductController::class, 'getUserProducts']);
+Route::controller(ProductController::class)->group(function () {
+    Route::post('/search-product', 'searchProduct');
+    Route::post('/sell-product', 'sellProduct');
+    Route::post('/edit-product', 'editProduct');
+    Route::post('/user-products', 'getUserProducts');
+    Route::post('/delete-product', 'deleteProduct');
+});
+
+Route::controller(CartController::class)->group(function () {
+    Route::post('/saveProduct-toCart', 'saveProduct');
+    Route::post('/saveShipping-toCart', 'saveShipping');
+    Route::post('/removeProduct-fromCart', 'removeProduct');
+    Route::post('/get-products', 'getCartProducts');
+    Route::post('/get-shipping', 'getCartShipping');
+});
+
+// Route::post('/search-product', [ProductController::class, 'searchProduct']);
+// Route::post('/sell-product', [ProductController::class, 'sellProduct']);
+// Route::post('/edit-product', [ProductController::class, 'editProduct']);
+// Route::post('/user-products', [ProductController::class, 'getUserProducts']);
+// Route::post('/saveProduct-toCart', [CartController::class, 'saveProduct']);
+// Route::post('/removeProduct-fromCart', [CartController::class, 'removeProduct']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
