ccb31d04650b9163ac194224adbdd7d3370a73dd
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-03-16 23:12:35 +0200

    fetch furniture and shippings

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -3,17 +3,62 @@ import { Button, Typography } from '@mui/material';
 import { TextField } from '@material-ui/core';
 import '../../css/cart/cart.css';
 import FurnitureItem from '../../components/furnitureItem';
+import ShippingProfileCard from '../../components/shippingProfileCard/shippingCardProfile';
 
 const Cart = ()=>{
 
     const[ClickShipping, setClickShipping]= useState(false);
     const[ClickSavedFurniture, setClickSavedFurniture]= useState(true);
 
+    const[savedProducts, setSavedProducts]= useState([]);
+    const[savedShipping, setShipping]= useState([]);
+
+
     const[savedFurnitureColor, setsavedFurnitureColor]= useState('#6F1A07'); 
     const[shippingColor, setshippingColor]= useState('#B4A89E');
 
+    let object={user_id: '622fb36b71260000aa0049fc'};
+
+    const getSavedProducts = async()=>{
+
+        await fetch("http://127.0.0.1:8000/api/get-products",{
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(object),
+            method: "POST",
+        })
+              .then((response)=> response.json())
+              .then((result)=>{
+                  setSavedProducts(result[0])
+                  console.log('called me')
+              })
+              .catch(e=>{console.log(e)})
+    }
+
+    const getShipping = async()=>{
+
+        await fetch("http://127.0.0.1:8000/api/get-shipping",{
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(object),
+            method: "POST",
+        })
+              .then((response)=> response.json())
+              .then((result)=>{
+                  setShipping(result[0])
+                  console.log('called me')
+              })
+              .catch(e=>{console.log(e)})
+    }
+
     useEffect(() => {
 
+        console.log(ClickShipping);
+        console.log(ClickSavedFurniture);
         if(ClickShipping)
         {
             setshippingColor('#6F1A07');
@@ -27,6 +72,11 @@ const Cart = ()=>{
         }
 
       },[ClickShipping, ClickSavedFurniture]);
+
+      useEffect(() => {
+        getSavedProducts();
+        getShipping();
+      },);
 
     return(
         <div className='cart-page'>
@@ -46,10 +96,23 @@ const Cart = ()=>{
 
             </div>
             <div className='buy-page-items'>
-            <FurnitureItem />
-            <FurnitureItem />
-            <FurnitureItem />
-            <FurnitureItem />
+            {ClickSavedFurniture &&  savedProducts.map((item)=>
+                <FurnitureItem 
+                title = {item['title']} 
+                description = {item.description}
+                location = {item.location}
+                price = {item.price}
+                date={item.created_at}
+                />
+                ) }
+            {ClickShipping &&  savedShipping.map((item)=>
+                <ShippingProfileCard 
+                name = {item.name} 
+                phone_number = {item.phone_number}
+                location = {item.location}
+                vehicle_load = {item.vehicle_load}
+                />
+                ) }
             </div>
             
         </div>
