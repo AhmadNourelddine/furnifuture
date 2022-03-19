a87497916137e2c368e72449f437c0a3de2bf532
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-03-19 09:39:01 +0200

    axios instead of fetch + added toggle logic for navbar

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Button, Typography } from '@mui/material';
 import { TextField } from '@material-ui/core';
 import '../../css/cart/cart.css';
@@ -7,76 +8,44 @@ import ShippingProfileCard from '../../components/shippingProfileCard/shippingCa
 
 const Cart = ()=>{
 
-    const[ClickShipping, setClickShipping]= useState(false);
-    const[ClickSavedFurniture, setClickSavedFurniture]= useState(true);
+    let token = window.localStorage.getItem('authToken');
+
+    const[toggle, setToggle]= useState(true);
 
     const[savedProducts, setSavedProducts]= useState([]);
     const[savedShipping, setShipping]= useState([]);
 
 
-    const[savedFurnitureColor, setsavedFurnitureColor]= useState('#6F1A07'); 
-    const[shippingColor, setshippingColor]= useState('#B4A89E');
-
-    let object={user_id: '622fb36b71260000aa0049fc'};
-
     const getSavedProducts = async()=>{
 
-        await fetch("http://127.0.0.1:8000/api/get-products",{
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(object),
-            method: "POST",
+        await axios.get('http://127.0.0.1:8000/api/user/cart/get-products',{
+            headers: {"Authorization" : `Bearer ${token}`} 
         })
-              .then((response)=> response.json())
-              .then((result)=>{
-                  setSavedProducts(result[0])
-                  console.log('called me')
-              })
-              .catch(e=>{console.log(e)})
+        .then((response)=>{
+                setSavedProducts(response.data[0])
+                console.log(response)
+            })
+        .catch(e=>{console.log(e)})
+
     }
 
     const getShipping = async()=>{
 
-        await fetch("http://127.0.0.1:8000/api/get-shipping",{
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(object),
-            method: "POST",
+        await axios.get('http://127.0.0.1:8000/api/user/cart/get-shipping',{
+            headers: {"Authorization" : `Bearer ${token}`} 
         })
-              .then((response)=> response.json())
-              .then((result)=>{
-                  setShipping(result[0])
-                  console.log('called me')
-              })
-              .catch(e=>{console.log(e)})
+        .then((response)=>{
+                setShipping(response.data[0])
+                console.log(response)
+            })
+        .catch(e=>{console.log(e)})
+
     }
-
-    useEffect(() => {
-
-        console.log(ClickShipping);
-        console.log(ClickSavedFurniture);
-        if(ClickShipping)
-        {
-            setshippingColor('#6F1A07');
-            setsavedFurnitureColor('#B4A89E');
-            setClickSavedFurniture(false);
-        }
-        if(ClickSavedFurniture){
-            setshippingColor('#B4A89E');
-            setsavedFurnitureColor('#6F1A07');
-            setClickShipping(false);
-        }
-
-      },[ClickShipping, ClickSavedFurniture]);
 
       useEffect(() => {
         getSavedProducts();
         getShipping();
-      },);
+      },[]);
 
     return(
         <div className='cart-page'>
@@ -86,17 +55,18 @@ const Cart = ()=>{
             <Typography className='cart-page-title'>Your Cart</Typography>
             <div className='cart-page-navbar'>
                 <Button className='cart-page-navbar-btn'
-                style={{backgroundColor:savedFurnitureColor}}
-                onClick={()=>{setClickSavedFurniture(true)}}>Saved Furniture</Button>
+                style={toggle? {backgroundColor: '#6F1A07'} : {backgroundColor: '#B4A89E'}}
+                onClick={()=>{setToggle(true)}}>Saved Furniture</Button>
 
                 <Button className='cart-page-navbar-btn' 
-                style={{backgroundColor:shippingColor}}
-                onClick={()=>{setClickShipping(true)}}>Shipping</Button>
+                style={toggle? {backgroundColor: '#B4A89E'} : {backgroundColor: '#6F1A07'}}
+                onClick={()=>{setToggle(false)}}>Shipping</Button>
+                
             </div>
 
             </div>
             <div className='buy-page-items'>
-            {ClickSavedFurniture &&  savedProducts.map((item)=>
+            {toggle &&  savedProducts.map((item)=>
                 <FurnitureItem 
                 title = {item['title']} 
                 description = {item.description}
@@ -105,7 +75,7 @@ const Cart = ()=>{
                 date={item.created_at}
                 />
                 ) }
-            {ClickShipping &&  savedShipping.map((item)=>
+            {!toggle &&  savedShipping.map((item)=>
                 <ShippingProfileCard 
                 name = {item.name} 
                 phone_number = {item.phone_number}
