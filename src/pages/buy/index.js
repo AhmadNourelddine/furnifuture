56cb348d93a0738758f698af96a583d97b0f9679
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-03-20 22:56:03 +0200

    handle search with category

--- src/pages/buy/index.js
+++ src/pages/buy/index.js
@@ -10,19 +10,48 @@ import FurnitureItem from '../../components/furnitureItem';
 
 const Buy = ()=>{ 
 
-    const [search, setSearch]= useState('');
-    const [data, setData]= useState([]);
+    const[searching, setSearching]= useState(false);
 
+    const [search, setSearch]= useState('');
+    const [categ, setCateg]= useState('');
+
+    const [data, setData]= useState([]);
+    const [result, setResult]= useState([]);
+
+    let token = window.localStorage.getItem('authToken');
+  
+    const category = [
+        { label: 'Living Room', id: 1 },
+        { label: 'Dining Room ', id: 2 },
+        { label: 'Bedroom', id: 3 },
+        { label: 'Kids', id: 4 }
+        ];
+    
+    let object = {
+        "search": search,
+        "category": categ
+    };
+    
     const getRandomProducts = async()=>{
 
         await axios.get('http://127.0.0.1:8000/api/random-products')
             .then((response)=>{
-                    setData(response.data[0])
-                    console.log(response)
+                    setData(response.data[0]);
+                    console.log(response);
                 })
             .catch(e=>{console.log(e)})
     }
 
+    const searchFurniture = async()=>{
+
+        await axios.post('http://127.0.0.1:8000/api/search-products', object)
+            .then((response)=>{
+                    setResult(response.data[0]);
+                    setSearching(true);
+                    console.log(response);
+                })
+            .catch(e=>{console.log(e)})
+    }
     useEffect(() => {
             getRandomProducts();
     },[]);
@@ -39,20 +68,20 @@ const Buy = ()=>{
                  />
                  <Autocomplete className='buy-search-category'
                     disablePortal
-                    // id="combo-box-demo"
-                    // options={top100Films}
+                    options={category}
                     sx={{ width: 300 }}
                     renderInput={(params) => <TextField
                         className="buy-page-search-category-textfield"
                          {...params} 
                          label="Category" />}
+                    onChange = {(event, value)=>{value && setCateg(value.label)}}
                     />
-                <Button className='buy-search-btn'>
+                <Button onClick={searchFurniture} className='buy-search-btn'>
                     Search
                 </Button>
             </div>
             <div className='buy-page-items'>
-            {            
+            {!searching &&           
                 data.map((item)=>
                 <FurnitureItem 
                 key = {item._id}
@@ -63,8 +92,20 @@ const Buy = ()=>{
                 price = {item.price}
                 date={item.created_at}
                 btn='save'
-                />
-                ) 
+                />) 
+            }
+            {searching && 
+                 result.map((item)=>
+                 <FurnitureItem 
+                 key = {item._id}
+                 id = {item._id}
+                 title = {item.title} 
+                 description = {item.description}
+                 location = {item.location}
+                 price = {item.price}
+                 date={item.created_at}
+                 btn='save'
+                 />) 
             }
             </div>
             
