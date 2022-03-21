a72e533717d11899aff58955b253688573ead830
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-03-21 03:09:25 +0200

    created profile page

---
+++ src/pages/profile/index.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Box, Button, Typography } from '@mui/material';
+import { TextField } from '@material-ui/core';
+import '../../css/profile/profile.css';
+import FurnitureItem from '../../components/furnitureItem';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import { Link } from 'react-router-dom';
+
+
+
+const Profile = ()=>{ 
+
+    const [data, setData]= useState([]);
+
+    let token = window.localStorage.getItem('authToken');
+    
+    const getRandomProducts = async()=>{
+
+        await axios.get('http://127.0.0.1:8000/api/user/product/created',{
+            headers: {"Authorization" : `Bearer ${token}`} 
+        })
+            .then((response)=>{
+                    setData(response.data['user_products']);
+                    console.log(response);
+                })
+            .catch(e=>{console.log(e)})
+    }
+
+    
+    useEffect(() => {
+            getRandomProducts();
+    },[]);
+
+    return(
+
+        <div className='profile-page'>
+            <Box className='profile-page-info'>
+                <Box><AccountCircleIcon sx={{fontSize:150}}/></Box>
+                <Box className='profile-page-name-email'>
+                    <Typography fontWeight={900} fontSize={50}>User Name</Typography>
+                    <Typography fontWeight={100} fontSize={30}>User Email</Typography>
+                </Box>
+                <Box  component={Link} to="/dashboard" className='profile-page-edit'><ManageAccountsIcon sx={{fontSize:45}}/> </Box>
+                </Box>
+            <div className='profile-page-items'>
+            {         
+                data.map((item)=>
+                <FurnitureItem 
+                key = {item._id}
+                id = {item._id}
+                title = {item.title} 
+                description = {item.description}
+                location = {item.location}
+                price = {item.price}
+                date={item.created_at}
+                btn='delete'
+                />) 
+            }
+            </div>
+            
+        </div>
+    );
+}
+
+export default Profile;
