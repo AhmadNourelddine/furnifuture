fec0217f948e063bda8a5b7646cc2f77cf215ffa
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-03-23 19:21:16 +0200

    added authshipping reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import authReducer from './authReducer';
+import authShippingReducer from './authShippingReducer';
 import {combineReducers} from 'redux';
 
 const allReducers = combineReducers({
     authReducer,
+    authShippingReducer,
 });
 
 export default allReducers;
