78171da68b76290344b20e050f21f15ecbcf20ee
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-03-23 23:59:45 +0200

    added protected route

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import CreateShippingProfile from './pages/create-shipping-profile/create-shippi
 import Cart from './pages/cart';
 import Profile from './pages/profile';
 import ProfileShipping from './pages/profile-shipping';
+import ProtectedRoutes from './protectedRoutes';
 
 function App() {
   return (
@@ -20,18 +21,20 @@ function App() {
       { 
         <div>
           <Routes>
+          <Route element={<ProtectedRoutes/>}>
+            <Route exact path="/sell" element={<Sell/>} />
+            <Route exact path="/profile" element={<Profile/>} />
+            <Route exact path="/profile-shipping" element={<ProfileShipping/>} />
+            <Route path="/dashboard" element={<UpdateProfile />} />
+            <Route exact path="/cart" element={<Cart/>} />
+          </Route>
           <Route exact path="/*" element={<About/>} />
           <Route exact path="/about" element={<About/>} />
           <Route exact path="/buy" element={<Buy/>} />
-          <Route exact path="/sell" element={<Sell/>} />
           <Route exact path="/delivery" element={<Delivery/>} />
-          <Route exact path="/cart" element={<Cart/>} />
-          <Route exact path="/profile" element={<Profile/>} />
-          <Route exact path="/profile-shipping" element={<ProfileShipping/>} />
           <Route exact path="/login" element={<Login/>} />
           <Route exact path="/signup" element={<SignUp/>} />
           <Route exact path="/contact-us" element={<ContactUs/>} />
-          <Route path="/dashboard" element={<UpdateProfile />} />
           <Route exact path="/create-shipping-profile" element={<CreateShippingProfile/>} />
           </Routes>
         </div>
