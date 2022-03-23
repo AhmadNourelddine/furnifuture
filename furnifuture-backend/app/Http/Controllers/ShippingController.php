fb503babb78d5cf9fc2a00edd6e399d5bae1c417
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-03-23 19:14:38 +0200

    created shipping controller

---
+++ furnifuture-backend/app/Http/Controllers/ShippingController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\User;
+use Validator;
+
+class ShippingController extends Controller
+{
+    public function searchShipping(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'search' => 'string',
+            'category' => 'string',
+            'location' => 'string',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        
+        $search_term = $request->search;
+        $location = $request->location;
+        $vehicle_load = $request->vehicle_load;
+
+        if(empty($location) && empty($vehicle_load)){
+            $results = User::all()->where('is_shipping','=','true')
+            ->where('name','=',$search_term)
+            ->orWhere('email','=',$search_term);   
+            return response()->json([$results]);
+        }
+        else if(empty($category)){
+            $results = User::all()->where('is_shipping','=','true')
+            ->where('name','=', $search_term)
+            ->orWhere('email','=', $search_term)
+            ->where('location', '=', $location);   
+            return response()->json([$results]);
+        }
+        else if(empty($location)){
+            $results = User::all()->where('is_shipping','=','true')
+            ->where('name','=', $search_term)
+            ->orWhere('email','=', $search_term)
+            ->where('category', '=', $category);   
+            return response()->json([$results]);
+        }
+        else{
+            $results = User::all()->where('is_shipping','=','true')
+            ->where('name','=', $search_term)
+            ->orWhere('email','=', $search_term)
+            ->where('location', '=', $location)  
+            ->where('category', '=', $category);   
+            return response()->json([$results]);
+        }
+
+    }
+
+    public function allShippings()
+    {
+        $shippings = User::all()->where('is_shipping','=','true');
+        return response()->json($shippings);
+    }
+}
+
+
+ // ->where(function($query) use($search_term, $location, $vehicle_load){
+                        //                     $query->where('name','=',$search_term)
+                        //                         ->orWhere('email','=',$search_term)
+                        //                         ->orWhere('location','=',$location)
+                        //                         ->orWhere('vehicle_load','=',$vehicle_load)
+                        //                         ->get();
+                        //                 });
+                        
+                        // ->where(function ($query) use($search_term, $location, $vehicle_load){
+                        //     $query->where('name','LIKE','%'.$search_term.'%')
+                        //           ->orWhere('email','LIKE','%'.$search_term.'%')
+                        //           ->orWhere('location','LIKE','%'.$location.'%')
+                        //           ->orWhere('vehicle_load','LIKE','%'.$vehicle_load.'%');
+                        // })
+                        // ->get();
