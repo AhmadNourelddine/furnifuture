ab868522820d2add2f62435b2c15cc7e64559974
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-03-24 18:09:28 +0200

    edit product action

---
+++ src/redux/actions/editProduct.js
@@ -0,0 +1,14 @@
+
+export const editProduct = (product)=>{
+    return{
+        type:'EditProduct',
+        payload: product,
+    };
+ };
+ 
+ export const removeProduct = (product)=>{
+    return{
+        type:'RemoveProduct',
+    };
+ };
+ 
