27d61632a92a9340271b3ceaaf7b8ed712b57956
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-03-24 18:10:13 +0200

    added edit product reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import authReducer from './authReducer';
 import authShippingReducer from './authShippingReducer';
+import editProductReducer from './editProductReducer';
 import {combineReducers} from 'redux';
 
 const allReducers = combineReducers({
     authReducer,
     authShippingReducer,
+    editProductReducer,
 });
 
 export default allReducers;
