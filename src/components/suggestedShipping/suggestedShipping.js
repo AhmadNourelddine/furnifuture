8bce9f10bee8e6b60b8158d7298b3ab71d86b9dd
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-03-25 21:03:03 +0200

    suggested shipping component

---
+++ src/components/suggestedShipping/suggestedShipping.js
@@ -0,0 +1,24 @@
+import { Box, Typography } from "@mui/material";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import CheckBoxOutlinedIcon from '@mui/icons-material/CheckBoxOutlined';
+
+const SuggestedShipping=(props)=>{
+    return (
+        <Box style={{display:'flex', flexDirection:'column', justifyContent:'flex-start'}} 
+                className='modal-shipping-profiles'>
+
+                <Box sx={{py:1}} style={{display:'flex', alignContent:'center'}}>
+                <Box><AccountCircleIcon sx={{fontSize:100, pr:1}}/></Box>
+                <Box >
+                    <Typography fontWeight={500} fontSize={10}>{props.name}</Typography>
+                    <Typography fontWeight={100} fontSize={10}>{props.location}</Typography>
+                    <Typography fontWeight={100} fontSize={10}>{props.phone_number}</Typography>
+                </Box>
+                <Box sx={{pl:3}} style={{alignSelf:'flex-start'}}><CheckBoxOutlinedIcon/></Box>
+                </Box>
+
+              </Box>
+    )
+}
+
+export default SuggestedShipping;
