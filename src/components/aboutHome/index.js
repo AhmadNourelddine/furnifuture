c3c9f97b5aa4f0dcf171e81967f01ad32d676b7b
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-03-29 04:24:42 +0300

    navigate when click buttons

--- src/components/aboutHome/index.js
+++ src/components/aboutHome/index.js
@@ -1,8 +1,11 @@
 import { Button, Typography } from '@mui/material';
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import '../../css/aboutHome/aboutHome.css';
 
 const AboutHome = ()=>{
+
+    const navigate = useNavigate();
 
     return(
         <div id='about-page-home'>
@@ -14,12 +17,14 @@ const AboutHome = ()=>{
                 <div id='about-buy-sell-getstarted'>
                     <p className="getstarted-title">Buy & Sell Furniture</p>
                     <p className="getstarted-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                    <Button className="getstarted-buysell-button">Get Started</Button>
+                    <Button component={Link} to="/signup"
+                    className="getstarted-buysell-button">Get Started</Button>
                 </div>
                 <div id='about-deliver-getstarted'>
                     <p className="getstarted-title">Deliver Furniture</p>
                     <p className="getstarted-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                    <Button className="getstarted-delivery-button">Get Started</Button>
+                    <Button component={Link} to="/create-shipping-profile"
+                    className="getstarted-delivery-button">Get Started</Button>
                 </div>
             </div>
         </div>
