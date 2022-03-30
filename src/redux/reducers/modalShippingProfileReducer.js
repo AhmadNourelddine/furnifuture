74a36134666cd9836191e3873711170d24cb2550
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-03-30 08:16:47 +0300

    shipping profile  modal reducer

---
+++ src/redux/reducers/modalShippingProfileReducer.js
@@ -0,0 +1,10 @@
+const modalShippingProfileReducer = (state=false, action)=>{
+    
+    if(action.type ==='OpenShippingProfileModal')
+    {return true;}
+    else if(action.type === 'CloseShippingProfileModal')
+    {return false;}
+    else return state;
+};
+
+export default modalShippingProfileReducer;
