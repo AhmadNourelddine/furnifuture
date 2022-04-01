ea20630547d75163bcd797aaeefff8199c11507c
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-04-01 12:50:11 +0300

    optimized search + suggest shipping

--- furnifuture-backend/app/Http/Controllers/ShippingController.php
+++ furnifuture-backend/app/Http/Controllers/ShippingController.php
@@ -13,7 +13,9 @@ class ShippingController extends Controller
     public function searchShipping(Request $request){
 
         $validator = Validator::make($request->all(), [
-            'search' => 'string',
+            'search' => 'string|nullable',
+            'location' => 'string|nullable',
+            'vehicle_load' => 'string|nullable',
         ]);
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
@@ -23,32 +25,61 @@ class ShippingController extends Controller
         $location = $request->location;
         $vehicle_load = $request->vehicle_load;
 
-        if(empty($location) && empty($vehicle_load)){
-            $results = User::all()->where('is_shipping','=','true')
-            ->where('name','=',$search_term);
-            // ->orwhere('email','=',$search_term);   
+        if(empty($location) && empty($vehicle_load) && !empty($search_term)){
+            $results = User::where('is_shipping','=',true)
+                            ->where('name','LIKE','%'.$search_term.'%')
+                            ->where('email','LIKE','%'.$search_term.'%')
+                            ->get();   
             return response()->json([$results]);
         }
-        else if(empty($category)){
-            $results = User::all()->where('is_shipping','=','true')
-            ->where('name','=', $search_term)
-            ->orWhere('email','=', $search_term)
-            ->where('location', '=', $location);   
+        else if(empty($vehicle_load) && !empty($search_term)){
+            $results = User::where('is_shipping','=',true)
+                            ->where('location','=', $location)
+                            ->where(function ($query) use($search_term){
+                                return $query 
+                                ->where('name','LIKE','%'.$search_term.'%')
+                                ->orWhere('email','LIKE','%'.$search_term.'%');
+                            })->get();
             return response()->json([$results]);
         }
-        else if(empty($location)){
-            $results = User::all()->where('is_shipping','=','true')
-            ->where('name','=', $search_term)
-            ->orWhere('email','=', $search_term)
-            ->where('category', '=', $category);   
+        else if(empty($location) && !empty($search_term)){
+            $results = User::where('is_shipping','=',true)
+            ->where('vehicle_load','=', $vehicle_load)
+            ->where(function ($query) use($search_term){
+                return $query 
+                ->where('name','LIKE','%'.$search_term.'%')
+                ->orWhere('email','LIKE','%'.$search_term.'%');
+            })->get();
+            return response()->json([$results]);
+        }
+        else if(!empty($location) && !empty($vehicle_load) && empty($search_term)){
+            $results = User::where('is_shipping','=',true)
+            ->where('location', '=', $location)  
+            ->where('vehicle_load', '=', $vehicle_load)
+            ->get();   
+            return response()->json([$results]);
+        }
+        else if(!empty($location) && !empty($vehicle_load) && !empty($search_term)){
+            $results = User::where('is_shipping','=',true)
+            ->where('location', '=', $location)  
+            ->where('category', '=', $category)
+            ->where(function ($query) use($search_term){
+                return $query 
+                ->where('name','LIKE','%'.$search_term.'%')
+                ->orWhere('email','LIKE','%'.$search_term.'%');
+            })->get();   
+            return response()->json([$results]);
+        }
+        else if(!empty($location) && empty($vehicle_load) && empty($search_term)){
+            $results = User::where('is_shipping','=',true)
+            ->where('location', '=', $location)
+            ->get();   
             return response()->json([$results]);
         }
         else{
-            $results = User::all()->where('is_shipping','=','true')
-            ->where('name','=', $search_term)
-            ->orWhere('email','=', $search_term)
-            ->where('location', '=', $location)  
-            ->where('category', '=', $category);   
+            $results = User::where('is_shipping','=',true)
+            ->where('vehicle_load', '=', $vehicle_load)
+            ->get();   
             return response()->json([$results]);
         }
 
@@ -56,7 +87,6 @@ class ShippingController extends Controller
 
     public function randomShippings()
     {
-        // $shippings = User::all()->where('is_shipping','=','true');
         $shippings = User::all()->where('is_shipping','=','true')->random(4);
         foreach($shippings as $shipping){
             if($shipping->image){
@@ -68,27 +98,22 @@ class ShippingController extends Controller
         return response()->json($shippings);
     }
 
-    public function suggestShippings()
-    {
-        $shippings = User::all()->where('is_shipping','=','true')->take(3);
+    public function suggestShippings(Request $request)
+    {   
+        $city_user = $request->city_user;
+        $city_product = $request->city_product;
+
+        if($city_user || $city_product){
+            $shippings = User::where('is_shipping','=','true')
+                                ->orWhere('location', $city_product)
+                                ->orWhere('location', $city_user)
+                                ->take(3)
+                                ->get();
+        }
+        if(!$shippings->count() || (empty($city_product) && empty($city_user)) ){
+            $shippings = User::all()->where('is_shipping','=','true')->random(3);
+        }
         return response()->json($shippings);
     }
 
 }
-
-
- // ->where(function($query) use($search_term, $location, $vehicle_load){
-                        //                     $query->where('name','=',$search_term)
-                        //                         ->orWhere('email','=',$search_term)
-                        //                         ->orWhere('location','=',$location)
-                        //                         ->orWhere('vehicle_load','=',$vehicle_load)
-                        //                         ->get();
-                        //                 });
-                        
-                        // ->where(function ($query) use($search_term, $location, $vehicle_load){
-                        //     $query->where('name','LIKE','%'.$search_term.'%')
-                        //           ->orWhere('email','LIKE','%'.$search_term.'%')
-                        //           ->orWhere('location','LIKE','%'.$location.'%')
-                        //           ->orWhere('vehicle_load','LIKE','%'.$vehicle_load.'%');
-                        // })
-                        // ->get();
