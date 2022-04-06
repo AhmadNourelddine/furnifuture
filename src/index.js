b82308c201adb31f5c8f8c3111782f5704ef3cca
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-04-06 20:16:51 +0300

    remove unused

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import "./index.css";
 import App from "./App";
 import store from "./app/store.js";
 import { Provider } from "react-redux";
-import reportWebVitals from "./reportWebVitals";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -19,8 +18,3 @@ ReactDOM.render(
 
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
