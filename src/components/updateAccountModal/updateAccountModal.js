82ddac4d9c98de75eeb40bf054f6db78c97ba50c
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-04-06 20:21:27 +0300

    import css file

--- src/components/updateAccountModal/updateAccountModal.js
+++ src/components/updateAccountModal/updateAccountModal.js
@@ -1,108 +1,142 @@
-import { Box, Button, TextField } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import Modal from 'react-modal';
-import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
-import { closeUpdateProfileModal } from '../../redux/actions/modal';
+import { Box, Button, TextField } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import Modal from "react-modal";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { closeUpdateProfileModal } from "../../redux/actions/modal";
 import "../../css/profile/profile.css";
 import "../../css/profileModal/profileModal.css";
-import ToastSuccess from '../toast/toast-success';
-import { loggedIn, updateUser } from '../../redux/actions/logIn';
+import "../../css/update-profile/update-profile.css";
+import ToastSuccess from "../toast/toast-success";
+import { loggedIn, updateUser } from "../../redux/actions/logIn";
 
 const customStyles = {
   content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    padding:'0px',
-    borderRadius:'35px',
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    padding: "0px",
+    borderRadius: "35px",
   },
 };
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
 const UpdateProfileModal = (props) => {
-
   const dispatch = useDispatch();
   const [modalIsOpen, setIsOpen] = useState(true);
 
-  const [name,setName]= useState('');
-  const [email,setEmail]= useState('');
-  const [password,setPassword]= useState('');
-  const [Cpassword,setCpassword]= useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [Cpassword, setCpassword] = useState("");
 
   function closeTheModal() {
     dispatch(closeUpdateProfileModal());
     setIsOpen(false);
   }
 
-  const user = useSelector(state=>state.authUserReducer);
+  const user = useSelector((state) => state.authUserReducer);
 
-  let token = window.localStorage.getItem('authToken');
+  let token = window.localStorage.getItem("authToken");
 
-  const update = async()=>{
-
-    console.log(email+password)
-    let info={
-      "name":name,
-      "email" : email,
-      "password": password,
-      "password_confirmation": Cpassword
-    }
-    await axios.post("http://127.0.0.1:8000/api/user/update-profile", info, {
+  const update = async () => {
+    console.log(email + password);
+    let info = {
+      name: name,
+      email: email,
+      password: password,
+      password_confirmation: Cpassword,
+    };
+    await axios
+      .post("http://127.0.0.1:8000/api/user/update-profile", info, {
         headers: {
-          'Authorization': 'Bearer '+token
+          Authorization: "Bearer " + token,
         },
-    })
-          .then((response)=>{
-            closeTheModal();
-            dispatch(updateUser(response.data.user));
-            ToastSuccess('Updated Profile Successfully');
-            console.log(response)})
-          .catch((err)=>{
-            console.log(err)
-          })
-  }
+      })
+      .then((response) => {
+        closeTheModal();
+        dispatch(updateUser(response.data.user));
+        ToastSuccess("Updated Profile Successfully");
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     setName(user.name);
     setEmail(user.email);
-  },[user]);
+  }, [user]);
 
   return (
-
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeTheModal}
-        style={customStyles}
-        contentLabel="Update Profile Modal"
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeTheModal}
+      style={customStyles}
+      contentLabel="Update Profile Modal"
+    >
+      <Box
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
       >
-        <Box style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
+        <Box className="update-profile-form" id="update-profile-box-modal">
+          <p id="update-profile">Update Account Info</p>
+          <TextField
+            value={name}
+            className="outlined-basic"
+            label="Name"
+            variant="outlined"
+            margin="dense"
+            onChange={(e) => setName(e.target.value)}
+          />
 
-        <Box className='update-profile-form' id="update-profile-box-modal">
-        <p id="update-profile">Update Account Info</p>
-        <TextField value={name} className="outlined-basic" label="Name" variant="outlined" margin="dense"
-        onChange = {e=>setName(e.target.value)}/>
+          <TextField
+            value={email}
+            className="outlined-basic"
+            label="Email"
+            variant="outlined"
+            margin="dense"
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-        <TextField value={email} className="outlined-basic" label="Email" variant="outlined" margin="dense"
-        onChange = {e=>setEmail(e.target.value)}/>
+          <TextField
+            className="outlined-basic"
+            label="Password"
+            variant="outlined"
+            margin="dense"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-        <TextField className="outlined-basic" label="Password" variant="outlined" margin="dense" type="password"
-        onChange = {e=>setPassword(e.target.value)}/>
+          <TextField
+            className="outlined-basic"
+            label="Confirm Password"
+            variant="outlined"
+            margin="dense"
+            type="password"
+            onChange={(e) => setCpassword(e.target.value)}
+          />
 
-        <TextField className="outlined-basic" label="Confirm Password" variant="outlined" margin="dense" type="password"
-        onChange = {e=>setCpassword(e.target.value)}/>
-
-        <Button onClick={update} variant="contained" id="update-profile-btn" fullWidth>Update</Button>
+          <Button
+            onClick={update}
+            variant="contained"
+            id="update-profile-btn"
+            fullWidth
+          >
+            Update
+          </Button>
         </Box>
-
-        </Box>
-     
-      </Modal>
+      </Box>
+    </Modal>
   );
-}
+};
 
 export default UpdateProfileModal;
